8a64f3996394ef776c665e7c1aa012b170d14caa
m-a-r-c-e-l-i-n-o <me@marceli.no>
2018-01-14 21:40:23 -0500

    [feat] Add configurable logging to server
    Currently, there is no way to control what the server outputs to
    the terminal. This commit allows for a custom log object to be
    passed into the server.

--- server/init/index.js
+++ server/init/index.js
@@ -1,23 +1,29 @@
 const Koa = require('koa')
 const serverDestroy = require('server-destroy')
+const bunyan = require('bunyan')
 
-const ethicalServer = (opts = {}) => {
-    const port = ( parseInt(opts.port) ? opts.port : 8080 )
+const defaultLogger = bunyan.createLogger({
+    name: 'ethical-server'
+})
+
+const ethicalServer = (config = {}) => {
+    const { log = defaultLogger } = config
     const app = new Koa()
     const api = {
         use: (middleware) => {
             const wrapper = async (ctx, next) => {
                 try {
                     await middleware(ctx, next)
-                } catch (e) {
-                    console.error(`Middleware (${middleware}): ${e.stack}`)
+                } catch (err) {
+                    log.error({ err, message: `Middleware (${middleware})` })
                     process.exit(1)
                 }
             }
             app.use(wrapper)
             return api
         },
-        listen: () => new Promise(resolve => {
+        listen: (port = 9090) => new Promise(resolve => {
+            log.info(`Starting Server on Port: ${port}`)
             const server = app.listen(port, () => {
                 serverDestroy(server)
                 const destroy = server.destroy
